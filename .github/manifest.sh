96401944cc0cbb88ce99d42764548291133370e3
Kaito Sinclaire <ks@rosenthalcastle.org>
2025-09-24 17:51:45 -0700

    Add APWorld manifest, update actions to accomodate

---
+++ .github/manifest.sh
@@ -0,0 +1,10 @@
+manifest_ver=7
+manifest_compat_ver=7
+world_ver=${1:1}
+
+verinfo="\"version\": \"$manifest_ver\", \"compatible_version\": \"$manifest_compat_ver\", "
+worldinfo=", \"world_version\": \"$world_ver\""
+json=`cat archipelago.json`
+json="${json/\{/\{$verinfo}"
+json="${json/\}/$worldinfo\}}"
+echo $json > archipelago.json
